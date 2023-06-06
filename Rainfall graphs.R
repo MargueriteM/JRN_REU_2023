473a39dcf0a63ee18cfd0f11be78530805595ae0
iuribe3 <uribeisa541@gmail.com>
2023-06-05 20:42:15 -0600

    Added monthly data graphs for each year
    I still need to change the daily data.

--- Rainfall graphs.R
+++ Rainfall graphs.R
@@ -83,16 +83,27 @@ ggplot(annualdata, aes(year, annual_rain))+
   geom_point()+
   facet_grid(.~year)
 
-#total monthly data
+#total monthly data for all years combined
 monthlydata <- biomet_all%>%
   group_by(month)%>%
   summarize(monthly_rain= sum(P_RAIN_1_1_1, na.rm = TRUE))
 print(monthlydata)
 
-#graph total monthly rainfall data 
+#graph total monthly rainfall data for all years combined
 ggplot(monthlydata, aes(month, monthly_rain))+
   geom_point()+
   facet_grid(.~month)
+
+#total monthly data for each year
+monthly_yearlydata <- biomet_all%>%
+  group_by(month, year)%>%
+  summarize(monthly_yearlyrain= sum(P_RAIN_1_1_1, na.rm = TRUE))
+print(monthly_yearlydata)
+
+#graph total monthly rainfall data for each year
+ggplot(monthly_yearlydata, aes(month, monthly_yearlyrain))+
+  geom_point()+
+  facet_grid(month~year)
 
 #total daily data
 dailydata <- biomet_all%>%
@@ -100,7 +111,7 @@ dailydata <- biomet_all%>%
   summarize(daily_rain= sum(P_RAIN_1_1_1, na.rm = TRUE))
 print(dailydata)
 
-#graph total monthly rainfall data 
+#graph total daily rainfall data 
 ggplot(dailydata, aes(doy, daily_rain))+
   geom_point()+
   facet_grid(.~doy)
